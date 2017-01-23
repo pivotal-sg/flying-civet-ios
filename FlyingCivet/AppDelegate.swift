e4839f3497936d0e6049e2f487051d66fb6a9170
Yifeng Hou <yhou@pivotal.io>
2017-01-23 10:13:03 +0800

    Add Firebase using Carthage
    Signed-off-by: Carlos Gavino <cgavino@pivotal.io>

--- FlyingCivet/AppDelegate.swift
+++ FlyingCivet/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  FlyingCivet
-//
-//  Created by pivotal on 10/1/17.
-//  Copyright © 2017 pivotal. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
